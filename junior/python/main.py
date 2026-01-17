69a05e364fa881388cdec4cc55994d7f800db571
IgnRamosr <ign.ramosr@duocuc.cl>
2026-01-16 23:31:59 -0300

    Se resuelve ejercicio probando y asegurando que todos los ejemplos se resuelvan correctamente

--- junior/python/main.py
+++ junior/python/main.py
@@ -3,10 +3,34 @@ import json
 
 
 def calculate_panels(panel_width: int, panel_height: int, roof_width: int, roof_height: int) -> int:
+
+
+##### Primero sacamos la cantidad de paneles que caben tanto verticalmente como horizontalmente
+    cty_normal_width_panels = roof_width // panel_width
+    cty_normal_height_panels = roof_height // panel_height
+    total = cty_normal_height_panels * cty_normal_width_panels
+
+### Continuamos sacando el ESPACIO utilizado en la altura y anchura
+
+    used_height_space = cty_normal_height_panels * panel_height
+    used_width_space = cty_normal_width_panels * panel_width
+
+### Después necesitamos sacar lo SOBRANTE de la altura
+
+    remaining_height_roof = roof_height - used_height_space
+
+### Por último evaluamos si el sobrante de altura es mayor o igual al ancho del panel 
+### con el fin de evaluar si se puede colocar un panel rotado
+    if remaining_height_roof >= panel_width:
+
+        rotated_height_panels = roof_width // panel_height
+        rotated_width_panels = remaining_height_roof // panel_width
+
+        total += rotated_height_panels * rotated_width_panels
+
+
     
-    # Implementa acá tu solución
-    
-    return 0
+    return total
 
 
 def run_tests() -> None:
